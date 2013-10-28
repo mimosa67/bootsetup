fbf63fc6d62286aab4c49ac6c9363fc4dd6f14d9
Cyrille Pontvieux <cyrille.pontvieux@april-waf.com>
2013-10-28 10:55:42 +0100

    Fix compilation script. Glade file does not talk about Lilo only anymore.

--- compile.sh
+++ compile.sh
@@ -7,8 +7,8 @@ for p in data/po/*.po; do
 	echo "Compiling $l language"
 	msgfmt $p -o build/$l.mo || exit 1
 done
-intltool-merge data/po/ -d -u data/salix-live-installer.desktop.in build/salix-live-installer.desktop || exit 1
-cat <<'EOF' > build/salix-live-installer
+intltool-merge data/po/ -d -u data/bootsetup.desktop.in build/bootsetup.desktop || exit 1
+cat <<'EOF' > build/bootsetup
 #!/bin/sh
-exec python /usr/share/salix-live-installer/salix-live-installer.py "$@"
+exec python /usr/share/bootsetup/bootsetup.py "$@"
 EOF
