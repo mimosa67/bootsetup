466cfad963f2b2bdd4fb587039beae3a78b7b68e
Cyrille Pontvieux <cyrille.pontvieux@april-waf.com>
2013-10-30 18:23:48 +0100

    Prepare Lilo functions.

--- src/lib/lilo.py
+++ src/lib/lilo.py
@@ -10,6 +10,7 @@ __license__ = 'GPL2+'
 import tempfile
 import shutil
 import os
+import salix_livetools_library as sltl
 
 class Lilo:
   
@@ -26,6 +27,43 @@ class Lilo:
   def getConfigurationPath(self):
     return os.path.join(self._tmp, "lilo.conf")
 
+  def _mountBootPartition(self, boot_partition):
+    """
+    Return the mount point
+    """
+    # TODO mount boot_partition
+    return ""
+
+  def _mountBootInBootPartition(self, mount_point):
+    # TODO mount /boot in boot_partition
+    pass
+
+  def _mountPartitions(self, partitions):
+    """
+    Return a list of mount points for each partition
+    """
+    # TODO mount other partitions
+    return []
+
+  def _createLiloSections(self, partitions, mountPointList):
+    """
+    Return a list of lilo section string for each partition.
+    There could be more section than partitions if there are multiple kernels.
+    """
+    # TODO identify kernel+initrd in each linux partition
+    return []
+
+  def _umountAll(self, mount_point, mountPointList):
+    # TODO umount all
+    pass
+
+  def _getFrameBufferConf(self):
+    """
+    Return the frame buffer configuration for this hardware.
+    """
+    # TODO identify framebuffer type
+    return "normal"
+
   def createConfiguration(self, mbr_device, boot_partition, partitions):
     """
     paritions format: [device, filesystem, boot type, label]
@@ -33,12 +71,12 @@ class Lilo:
     self.mbr_device = mbr_device
     self.boot_partition = boot_partition
     self.partitions = partitions
-    # TODO mount boot_partition
-    # TODO mount /boot in boot_partition
-    # TODO mount other partitions
-    # TODO identify kernel+initrd in each linux partition
-    # TODO umount all
-    # TODO identify framebuffer type
+    mp = self._mountBootPartition(boot_partition)
+    self._mountBootInBootPartition(mp)
+    mpList = self._mountPartitions(partitions)
+    liloSections = self._createLiloSections(partitions, mpList)
+    self._umountAll()
+    fb = self._getFrameBufferConf()
     # TODO write configuration
     f = open(self.getConfigurationPath(), "w")
     f.write("""# LILO Configuration file
@@ -51,10 +89,10 @@ class Lilo:
     Assuming that last configuration editing didn't modified mount point.
     """
     print "TODO install LiLo"
-    # TODO mount boot_partition
-    # TODO mount /boot in boot_partition
+    mp = self._mountBootPartition(boot_partition)
+    self._mountBootInBootPartition(mp)
     # TODO bind /dev in boot_partition
-    # TODO mount other partitions
+    mpList = self._mountPartitions(partitions)
     # TODO copy the configuration to the boot_partition
     # TODO run lilo
-    # TODO umount all
+    self._umountAll()
